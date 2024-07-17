719e2f16c2baf2aaa355a2ab11c3aaba5f42e5ea
Michael Thomas <michaeljthomas97@yahoo.com>
2024-07-17 22:21:52 +0100

    Update JsonSerializerServiceTests.cs

--- StarWarsAPI.Tests/JsonSerializerServiceTests.cs
+++ StarWarsAPI.Tests/JsonSerializerServiceTests.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using Moq;
+﻿using Moq;
 using StarWarsAPI.Model;
 using StarWarsAPI.Services;
 
