bc2ffe76e2ec3ac8b6d1a03d4445f60994a5f4bf
Michael Thomas <michaeljthomas97@yahoo.com>
2024-07-17 14:28:58 +0100

    Setup Film and Characters classes

---
+++ Model/Characters.cs
@@ -0,0 +1,22 @@
+﻿namespace StarWarsAPI.Model
+{
+    public class Character
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Height { get; set; } = string.Empty;
+        public string Mass { get; set; } = string.Empty;
+        public string HairColor { get; set; } = string.Empty;
+        public string SkinColor { get; set; } = string.Empty;
+        public string EyeColor { get; set; } = string.Empty;
+        public string BirthYear { get; set; } = string.Empty;  
+        public string Gender { get; set; } = string.Empty;
+        public string Homeworld { get; set; } = string.Empty;
+        public List<string> Films { get; set; } = new List<string>();
+        public List<string> Species { get; set; } = new List<string>();
+        public List<string> Vehicles { get; set; } = new List<string>();
+        public List<string> Starships { get; set; } = new List<string>();
+        public string Created { get; set; } = string.Empty;
+        public string Edited { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+    }
+}
---
+++ Model/Film.cs
@@ -0,0 +1,20 @@
+﻿namespace StarWarsAPI.Model
+{
+    public class Film
+    {
+        public string Title { get; set; } = string.Empty;
+        public int EpisodeId { get; set; }
+        public string OpeningCrawl { get; set; } = string.Empty;
+        public string Director { get; set; } = string.Empty;
+        public string Producer { get; set; } = string.Empty;
+        public string ReleaseDate { get; set; } = string.Empty;
+        public List<string> Characters { get; set; } = new List<string>();
+        public List<string> Planets { get; set; } = new List<string>();
+        public List<string> Starships { get; set; } = new List<string>();
+        public List<string> Vehicles { get; set; } = new List<string>();
+        public List<string> Species { get; set; } = new List<string>();
+        public string Created { get; set; } = string.Empty;
+        public string Edited { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+    }
+}
