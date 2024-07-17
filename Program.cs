febf0e2e47462aef3464852d9d7e2b700945cb7c
Michael Thomas <michaeljthomas97@yahoo.com>
2024-07-17 16:16:16 +0100

    Serialising Films and Characters to json

--- Program.cs
+++ Program.cs
@@ -15,3 +15,33 @@ var serializedFilms = jsonSerializerService.Serialize(films);
 Console.WriteLine(serializedFilms);
 
 File.WriteAllText(@"C:\dev\Tests\StarWarsAPI\Data\films.json", serializedFilms);
+
+
+//Get Characters from each film
+
+// API request for each character -> cache results so don't do duplicate calls
+
+var characterUrlCache = new HashSet<string>();
+
+foreach (var film in films.Results)
+{
+    var serializedFilm = jsonSerializerService.Serialize(film);
+    Console.WriteLine(serializedFilm);
+    File.WriteAllText($"C:\\dev\\Tests\\StarWarsAPI\\Data\\Films\\{film.Title}.json", serializedFilm);
+
+    foreach (var character in film.Characters)
+    {
+        characterUrlCache.Add(character);
+    }
+}
+
+//Now we want API call for each character
+
+foreach (var character in characterUrlCache)
+{
+    var characterResponse = await apiService.GetAsync<Character>(character);
+    Console.WriteLine(characterResponse);
+    var serializedCharacter = jsonSerializerService.Serialize(character);
+    Console.WriteLine(serializedCharacter);
+    File.WriteAllText($"C:\\dev\\Tests\\StarWarsAPI\\Data\\Characters\\{characterResponse.Name}.json", serializedFilms);
+}
